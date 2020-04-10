8bd05fe8ac967604fc94bde2ca8ece79d890d80e
Anton Yasnov <maza4aka@icloud.com>
2020-04-10 17:40:25 +0300

    Laboratory work №2.2
    + реализация FFT
    + проверка правильности алгоритма и графики

--- fourier-transform.py
+++ fourier-transform.py
@@ -36,6 +36,7 @@ def generate_signal(harmonics, frequency, sampling):
 def discrete_fourier_transform(signal):
     """
     Дискретное преобразование Фурье.
+    Сложность: О(n^2)
     F(p) = sum( x(k)*W_N^(p*k) ), where W_N^(p*k) = e^(i*(-2pi/N)*p*k) = cos( (2pi/N)*p*k ) - i*sin( (2pi/N)*p*k )
     :param signal: сигнал для преобразования
     :return: результат F.
@@ -43,12 +44,45 @@ def discrete_fourier_transform(signal):
 
     N = len(signal)
 
+    W = lambda p, k: complex(np.cos( (2*np.pi*p*k)/N ), - np.sin( (2*np.pi*p*k)/N ))
+
     # массив F
     F = np.zeros(N, dtype=complex)
 
     for p in range(N):
         for k in range(N):
-            F[p] += complex(signal[k] * np.cos( (2*np.pi*p*k)/N ), - signal[k] * np.sin( (2*np.pi*p*k)/N ))
+            F[p] += signal[k] * W(p, k)
+
+    return F
+
+
+def fast_fourier_transform(signal):
+    """
+    Быстрое преобразование Фурье!
+    Модификация алгоритма выше, со сложностью O(nlog(n))
+    =P
+    """
+
+    #return np.fft.fft(signal)
+
+    N = len(signal)
+
+    # массив F
+    F = np.zeros(N, dtype=complex)
+
+    if N % 2 == 0: # должно быть степенью двух.
+        even = fast_fourier_transform(signal[0::2])
+        odd = fast_fourier_transform(signal[1::2])
+
+        W = lambda k: np.exp(-2j*np.pi*k/N)
+
+        for k in range(N//2):
+            F[k] = even[k] + W(k) * odd[k]
+            F[k + N//2] = even[k] - W(k) * odd[k]
+    elif N == 1: # замыкаем рекурсию...
+        return signal
+    else:
+        raise ValueError("размер массива signal должен быть степенью 2`ки!")
 
     return F
 
@@ -67,23 +101,29 @@ if __name__ == '__main__':
     # степень дискретизации (количество точек на графике)
     sampling_rate = 256
 
-    #signal = np.sin(np.linspace(0, np.pi*64, sampling_rate))
     signal = generate_signal(number_of_harmonics, cutoff_frequency, sampling_rate)
 
     # получаем данные для N синусоид
     DFT = discrete_fourier_transform(signal)
+    FFT = fast_fourier_transform(signal)
+
+    # проверим, правильно ли работает алгоритм FFT?..
+    assert np.allclose(DFT, FFT), "DFT и FFT дали разные результаты?"
+    DFT = FFT
 
     fig, axs = plt.subplots(3, 1, constrained_layout=True)
 
-    axs[1].set_title('Дискретное преобразование Фурье\n')
-    axs[1].plot([np.sqrt(z.real*z.real + z.imag*z.imag)/len(DFT) for z in DFT]) # амплитудный спектр
-    axs[2].plot([np.arctan(z.imag/z.real) for z in DFT]) # спектр фаз
+    x = np.arange(0, sampling_rate, 1)
 
-    axs[1].set_xlabel('синусоида')
+    axs[1].set_title('Быстрое преобразование Фурье...\nThe Cooley-Tukey algorithm!\n')
+    axs[1].bar(x, [np.sqrt(z.real*z.real + z.imag*z.imag)/len(DFT) for z in DFT]) # амплитудный спектр: |X_k|/N = sqrt(z.real^2 + z.imag^2)/N
+    axs[2].bar(x, [np.arctan(z.imag/z.real) for z in DFT]) # спектр фаз: phi = arctg( z.imag/z.real )
+
+    axs[1].set_xlabel('n`ая синусоида')
     axs[1].set_ylabel('амплитуда')
     axs[1].grid(True)
 
-    axs[2].set_xlabel('синусоида')
+    #axs[2].set_xlabel('n`ая синусоида')
     axs[2].set_ylabel('фаза')
     axs[2].grid(True)
 
